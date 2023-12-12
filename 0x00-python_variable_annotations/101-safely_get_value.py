f9bb83275e250ee4dd99a8b04466ecf72e2f8433
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-12-12 21:41:27 +0300

    using any and sequence

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -8,6 +8,7 @@ T = TypeVar("T")
 def safely_get_value(
     dct: Mapping, key: Any, default: Union[T, None] = None
 ) -> Union[Any, T]:
+    """returns a dict if key is in dict"""
     if key in dct:
         return dct[key]
     else:
