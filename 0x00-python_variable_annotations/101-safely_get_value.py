b3f11c6dc2dc309d323482e2035c9cc2e5b7131a
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-12-12 21:36:35 +0300

    using any and sequence

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -6,7 +6,7 @@ T = TypeVar("T")
 
 
 def safely_get_value(
-    dct: Mapping, key: Any, default: Union[T, None]
+    dct: Mapping, key: Any, default: Union[T, None] = None
 ) -> Union[T, None]:
     if key in dct:
         return dct[key]
