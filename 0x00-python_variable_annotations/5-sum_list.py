48cc9fceec45370ad5aa391997e77cf15305a6dd
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-12-12 20:35:36 +0300

    list with floats and ints

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""complex floats. List with floats"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """sum of values of list is returned"""
+    return sum(input_list)
---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""list with mixed integers and floats"""
+
+
+def sum_mixed_list(mxd_lst: list[float | int]) -> float:
+    """sum of values of mixed lists"""
+    return sum(mxd_lst)
