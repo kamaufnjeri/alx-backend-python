32c1060472b0e8d1f5b443809ed8c1fa2e787eec
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-12-12 21:34:35 +0300

    using any and sequence

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Using TypeVar"""
+from typing import TypeVar, Mapping, Any, Union
+
+T = TypeVar("T")
+
+
+def safely_get_value(
+    dct: Mapping, key: Any, default: Union[T, None]
+) -> Union[T, None]:
+    if key in dct:
+        return dct[key]
+    else:
+        return default
