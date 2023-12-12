36c619f4dd25f40bfc6c67d5e656b380b2cb5f95
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-12-12 21:22:56 +0300

    using any and sequence

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """duck type annotation"""
-from typing import Union, Sequence, Any
+from typing import Union, Sequence, Any, Optional
 
 
 # The types of the elements of the input are not know
-def safe_first_element(lst: Sequence) -> Union[Any, None]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     if lst:
         return lst[0]
     else:
