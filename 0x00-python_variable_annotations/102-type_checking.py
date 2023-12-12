41bf209d4cde5c0fd0d72ad216580db45ec42fd7
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-12-12 21:50:44 +0300

    using any and sequence

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,13 +1,19 @@
-def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
-    zoomed_in: Tuple = [
+#!/usr/bin/env python3
+"""using mypy"""
+from typing import Tuple, List
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """list with int values"""
+    zoomed_in: List = [
         item for item in lst
         for i in range(factor)
     ]
     return zoomed_in
 
 
-array = [12, 72, 91]
+array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 
-zoom_3x = zoom_array(array, 3.0)
+zoom_3x = zoom_array(array, 3)
