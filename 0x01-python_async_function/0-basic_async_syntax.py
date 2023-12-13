b87edb0aa62f1668b7ccf835795d9d2b8bafbc89
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-12-13 15:43:08 +0300

    using async and await

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,6 +5,7 @@ import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
+    """return the the time of await"""
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
